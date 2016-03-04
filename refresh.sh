38894751669c832eb5d472bbe51e5189d4a5d36c
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-03-04 09:45:01 +0000

    Built my own base
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 # First pull down the latest versions of base envs
-docker pull fedora:latest
+#docker pull fedora:latest
 
 # Now build all envs
 for env in base base-f22 base-f23 base-el7 base-el6 nodejs ipsilon ipsilon-f22 ipsilon-f23 ipsilon-el7 ipsilon-el6 python packaging;
