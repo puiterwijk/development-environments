3503a7c8ce7f06d03aaa715cb97e6e9f316c2bb3
Patrick Uiterwijk <puiterwijk@redhat.com>
2017-06-15 16:38:05 +0200

    Turns out that system_u:system_r is fine, and the level was the problem
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- docker/krb5/functions.sh
+++ docker/krb5/functions.sh
@@ -2,7 +2,7 @@
 function dkcmd() {
     cmd=$1
     shift
-    sudo docker run --rm=true -it --user docker --security-opt seccomp=$HOME/Documents/Development/Environments/docker/krb5/policy.json --cap-drop=ALL --security-opt="label=user:unconfined_u" --security-opt="label=role:unconfined_r" --security-opt="label=level:s0-s0:c0.c1023" --entrypoint=/bin/$cmd puiterwijk.org/development/krb5 $@
+    sudo docker run --rm=true -it --user docker --security-opt seccomp=$HOME/Documents/Development/Environments/docker/krb5/policy.json --cap-drop=ALL --security-opt="label=level:s0-s0:c0.c1023" --entrypoint=/bin/$cmd puiterwijk.org/development/krb5 $@
 }
 
 alias dklist="dkcmd klist"
